6cfaa92b5074d11228496ace055be19ee3261b56
agsyafiq55 <agsyafiq55@gmail.com>
2025-05-06 11:23:41 +0800

    Updated head.blade.php to add csrf tag

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,12 @@
 
 <title>{{ $title ?? 'KKHSbog' }}</title>
 
+{{-- ✅ CSRF Token --}}
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+{{-- ✅ Livewire Styles --}}
+@livewireStyles
+
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
